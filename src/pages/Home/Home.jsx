04da017686b196cd4b65c7e19a97230de0e6a87d
sahalmahin <samirsatter0142@gmail.com>
2024-06-04 11:53:30 +0600

    initial-commit

---
+++ src/pages/Home/Home.jsx
@@ -0,0 +1,17 @@
+import Banner from "./Banner";
+import Blogs from "./Blogs";
+import TopRated from "./TopRated";
+import Vision from "./Vision";
+
+const Home = () => {
+    return (
+        <div>
+            <Banner />
+            <Blogs />
+            <TopRated />
+            <Vision />
+        </div>
+    );
+};
+
+export default Home;
